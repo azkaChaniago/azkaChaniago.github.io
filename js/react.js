57477d63ded97fc8aff4e44341b0aaca844e53eb
Azka <azka24chaniago@gmail.com>
2019-06-28 11:31:42 +0700

    Start over with bootstrap

--- js/react.js
+++ js/react.js
@@ -1,94 +1,108 @@
 // Body
 class Body extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    }
+  }
+
   render() {
     return (
       <div>
         <Navbar />
-        <Jumbotron />
-        <About />
+        <div className="row">
+          <Aside />
+          <div className="col-md-10">
+            <h1>This gonna be Main Content</h1>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
+}
+
+// Aside
+class Aside extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <div className="col-md-2">
+        <ul className="list-group list-group-flush">
+          <AsideList classStyles="list-group-item"
+            linkName="about.html"
+            itemName="About"/>
+        </ul>
+      </div>
+    );
+  }
+}
+
+// Aside items
+const AsideList = props => {
+  return (
+    <li className={props.classStyles}>
+      <a href={props.linkName}>
+        {props.itemName}
+      </a>
+    </li>
+  );
 }
 
 // Navbar
 class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      blah: [
+        {name: "About", link: "about.html"},
+        {name: "Skills", link: "skills.html"},
+        {name: "Education", link: "education.html"},
+        {name: "Experiences", link: "experiences.html"},
+        {name: "Achievements", link: "achievements.html"},
+      ]
+    } 
+  }
+
   render() {
+    const nav_items = this.state.blah.map( (item, key) => {
+      return (
+        <NavItem key={key}
+          link={item.link}
+          itemName={item.name}
+          active={false}/>
+      );
+    });
+    console.log(nav_items);
     return (
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <Nav />
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            {nav_items}
+          </div>
         </div>
       </nav>
-    )
+    );
   }
 }
 
-// Nav
-class Nav extends React.Component {
-  render() {
-    return (
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div className="navbar-nav">
-          <NavItem style="nav-item nav-link" url="#" linkName="About"/>
-          <NavItem style="nav-item nav-link" url="#" linkName="Skills"/>
-          <NavItem style="nav-item nav-link" url="#" linkName="Education"/>
-          <NavItem style="nav-item nav-link" url="#" linkName="Experience"/>
-        </div>
-      </div>
-    )
-  }
-}
-
-// Nav Item
-const NavItem = props => <a className={props.style} href={props.url}>{props.linkName}</a>;
-
-// Jumbotron
-class Jumbotron extends React.Component {
-  render() {
-    return (
-      <div className="jumbotron jumbotron-fluid">
-        <div className="container">
-          <h1 className="display-4">Fluid jumbotron</h1>
-          <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-        </div>
-      </div>
-    )
-  }
-}
-
-// About Section
-class About extends React.Component {
-  render() {
-    return (
-      <div className="container">
-        <Card quote="Hello.." text="I'm an enthusiasm web developer. Currently working as Web Programming and Object Oriented Programming instructor in Universitas Teknokrat Indonesia." blockquote="Muhammad Fadhil Azka" source="my brain"/>
-      </div>
-    )
-  }
-}
-
-// Card
-class Card extends React.Component {
-  render() {
-    return (
-      <div class="card">
-        <div class="card-header">
-          {this.props.quote}
-        </div>
-        <div class="card-body">
-          <blockquote class="blockquote mb-0">
-            <p>{this.props.text}</p>
-            <footer class="blockquote-footer">{this.props.blockquote}<cite title="Source Title"> {this.props.source}</cite></footer>
-          </blockquote>
-        </div>
-      </div>
-    )
-  }
+// NavItem
+const NavItem = (props) => {
+  const active = props.active == true ? "nav-item nav-link active" : "nav-item nav-link";
+  return(
+    <a className={active} href={props.link}>
+      {props.itemName}
+      {props.active == true ? <span className="sr-only">(current)</span> : ""}
+    </a>
+  );
 }
 
 const element = <Body />;
